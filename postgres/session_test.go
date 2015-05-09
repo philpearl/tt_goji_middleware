5b5c18165e1927fbe23707d941bf89572f23488e
Philip Pearl <phil@insidernine.com>
2015-05-09 17:17:07 +0100

    beef up postgres session test

--- postgres/session_test.go
+++ postgres/session_test.go
@@ -92,6 +92,36 @@ func TestSessionUpdate(t *testing.T) {
 	sh.Destroy(c, s1)
 }
 
+func TestSessionResetTTL(t *testing.T) {
+	db, err := sql.Open("postgres", "postgres://test:test@localhost:/test?sslmode=disable")
+	if err != nil {
+		t.Skipf("Cannot connect to postgres. %v", err)
+	}
+	c := web.C{
+		Env: map[interface{}]interface{}{"db": db},
+	}
+	sh, err := NewSessionHolder(db)
+	if err != nil {
+		t.Fatalf("failed to create session holder - %v", err)
+	}
+
+	// Create the session
+	s := sh.Create(c)
+
+	err = sh.Save(c, s)
+	if err != nil {
+		t.Fatalf("failed to save session - %v", err)
+	}
+
+	// Just be sure the call doesn't fail
+	err = sh.ResetTTL(c, s)
+	if err != nil {
+		t.Fatalf("Failed to reset ttl - %v", err)
+	}
+
+	sh.Destroy(c, s)
+}
+
 func TestSessionCreate(t *testing.T) {
 	db, err := sql.Open("postgres", "postgres://test:test@localhost:/test?sslmode=disable")
 	if err != nil {

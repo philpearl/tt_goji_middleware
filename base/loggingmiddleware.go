cc3f49392b8e90b7f6ff93c87c587721e76bdac3
Phil Pearl <phil@tanktop.tv>
2016-06-18 11:59:52 +0100

    Log web responses with JSON

--- base/loggingmiddleware.go
+++ base/loggingmiddleware.go
@@ -1,6 +1,7 @@
 package base
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"time"
@@ -31,6 +32,42 @@ func LoggingMiddleWare(c *web.C, h http.Handler) http.Handler {
 			remoteAddr = fwd + ":" + r.Header.Get("X-Forwarded-Port")
 		}
 		log.Printf("%s - %s %s %d %dms\n", remoteAddr, r.Method, r.RequestURI, ww.Status, time.Since(start).Nanoseconds()/1000000)
+	}
+	return http.HandlerFunc(handler)
+}
+
+type jsonLog struct {
+	RemoteAddr     string `json:"remote_addr"`
+	Method         string `json:"method"`
+	RequestURI     string `json:"url"`
+	Status         int    `json:"status"`
+	ResponseTimeMs int64  `json:"response_ms"`
+}
+
+func LoggingMiddleWareJSON(c *web.C, h http.Handler) http.Handler {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		ww := &StatusTrackingResponseWriter{w, http.StatusOK}
+		h.ServeHTTP(ww, r)
+
+		l := jsonLog{}
+		fwd := r.Header.Get("X-Forwarded-For")
+		if fwd == "" {
+			l.RemoteAddr = r.RemoteAddr
+		} else {
+			l.RemoteAddr = fwd + ":" + r.Header.Get("X-Forwarded-Port")
+		}
+		l.Method = r.Method
+		l.RequestURI = r.RequestURI
+		l.Status = ww.Status
+		l.ResponseTimeMs = time.Since(start).Nanoseconds() / 1000000
+
+		data, err := json.Marshal(&l)
+		if err != nil {
+			log.Printf("Failed to marshal JSON log. %v", err)
+		} else {
+			log.Println(string(data))
+		}
 	}
 	return http.HandlerFunc(handler)
 }
